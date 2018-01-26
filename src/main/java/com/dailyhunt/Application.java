ba6997c340e2ea1711d3104682a87a60078cac23
mukesh.bansal <mukesh.bansal@dailyhunt.in>
2018-01-26 19:01:16 +0530

    first Commit

---
+++ src/main/java/com/dailyhunt/Application.java
@@ -0,0 +1,19 @@
+package com.dailyhunt;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackageClasses=com.dailyhunt.controller.MainController.class)
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        SpringApplication.run(Application.class, args);
+
+
+	}
+
+}
